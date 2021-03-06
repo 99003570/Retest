1870ae136ab9e1319de0741a7bfc37d35e08c51d
99003570 <78849735+99003570@users.noreply.github.com>
2021-03-06 18:10:24 +0530

    Add files via upload

---
+++ 99003570_Retest/Food_Ordering_App/ConsumerOrder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Food_Ordering_App
+{
+    public class ConsumerOrder : Order
+     {
+
+        public double CheckoutConsumer(int intFriesNum, int intBurgerNum, string strNameInput)
+        {
+            //string strNameInput;
+            CustomerType = CustomerType.CONSUMER;
+            strCustomerName = strNameInput;
+            intNumberOfFries = intFriesNum;
+            intNumberOfBurger = intBurgerNum;
+            intTotalItems = intFriesNum + intBurgerNum;
+            decFriesubtotal = intFriesNum * decPriceFries;
+            decBurgerSubtotal = intBurgerNum * decPriceSand;
+            decSubtotal = decFriesubtotal + decBurgerSubtotal;
+
+            double res1 = CalcTotalsCon();
+            return res1;
+        }
+
+        //method to calculate the totals for sales tax and the total
+        public double CalcTotalsCon()
+        {
+            decSalesTax = decTaxRate * decSubtotal;
+            return decSubtotal + decSalesTax;
+        }
+
+        //overriding the ToString Method for this class
+        public override string ToString()
+        {
+            return "Customer Type: " + CustomerType + "\n" + "Customer name: " + strCustomerName + "\n"
+                + "Total Items: " + intTotalItems.ToString() + "\nFries Subtotal: " + decFriesubtotal.ToString() + "\nBurger Subtotal: " + decBurgerSubtotal.ToString()
+                + "\nSubtotal: " + decSubtotal.ToString() + "\nSales Tax: " + decSalesTax.ToString() + "\nGrand Total: " + CalcTotalsCon().ToString();
+        }
+    }
+}
---
+++ 99003570_Retest/Food_Ordering_AppTests/CateringOrderTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Food_Ordering_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Ordering_App.Tests
+{
+    [TestClass()]
+    public class CateringOrderTests
+    {
+        [TestMethod()]
+        public void CheckoutCateringTest()
+        {
+            double expected = 540;
+            CateringOrder CObj = new CateringOrder();
+            double actual = CObj.CheckoutCatering(10, 10, "sandy", "100");
+        }
+    }
+}
+
