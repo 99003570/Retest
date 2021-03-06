a4c9a4657301065135e5a0253eb43916047d2248
99003570 <78849735+99003570@users.noreply.github.com>
2021-03-07 02:12:37 +0530

    Add files via upload

---
+++ Food_Ordering_AppTests/ConsumerOrderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Food_Ordering_App;
+namespace Food_Ordering_AppTests
+{
+    [TestClass()]
+    public class VisitorOrderTests
+    {
+        [TestMethod()]
+        public void CheckoutVisitorTest()
+        {
+            double expected = 440;
+            VisitorOrder CObj = new VisitorOrder();
+            double actual = CObj.CheckoutVisitor(10, 10,"Ram");
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
